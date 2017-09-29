feb3b6838488a621150d055d2a26834168b5cd7e
mulleer <mulleeryry@gmail.com>
2017-09-29 14:57:05 -0400

    to right and left

--- student.py
+++ student.py
@@ -79,8 +79,10 @@ class Piggy(pigo.Pigo):
     def now_kick(self):
         for x in range(2):
             self.encF(5)
+            self.servo(30)
             self.encR(20)
             self.encF(5)
+            self.servo(150)
             self.encR(20)
 
 
